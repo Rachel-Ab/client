594d64f505a88bc69589671dfc02569aabcfa40e
laurent <l.neveux@icloud.com>
2022-02-24 17:06:21 +0100

    exaplanations

--- client/src/components/dashboard/AdminProducts.js
+++ client/src/components/dashboard/AdminProducts.js
@@ -2,19 +2,32 @@ import React, { useContext } from 'react';
 
 // Les champs name pour créer un prod category_id, title, metaDescription, description, priceHT
 // + image
+
 const AdminProducts = () => {
   return (
-    <div>
-      Page admin products
-      <p>Recupérer tous les produits</p>
-      <p>Faire un form pour créer un produit</p>
-      <p>Faire un form pour mettre a jour les produits</p>
-      <p>
-        Quand on clique sur un produit de la liste, le formulaire de pré-remplit
-      </p>
-      <p>
-        Récuperer les categories pour en associer une au produit que l'on créer
-      </p>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+          <h2 className="h2">Page admin products</h2>
+        </div>
+        <p>Recupérer tous les produits</p>
+        <p>Faire un form pour créer un produit</p>
+        <p>Faire un form pour mettre a jour les produits</p>
+        <p>
+          Quand on clique sur un produit de la liste, le formulaire de
+          pré-remplit
+        </p>
+        <p>
+          Récuperer les categories pour en associer une au produit que l'on
+          créer
+        </p>
+      </div>
+      <form>
+        <input type="text" name="title" />
+        <input type="text" name="metaDescription" />
+        <input type="text" name="description" />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
